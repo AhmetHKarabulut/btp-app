a60175a4c0c07f8c5128027e544f29016241a887
OpusMagnum <harunarman@outlook.com>
2026-01-29 12:29:36 +0300

    Wipe repository content

--- src/api/releaseNotes.js
+++
@@ -1,8 +0,0 @@
-import { requestWithRetry } from './client';
-
-const NEWEST_PATH = '/api/ReleaseNotes/GetNewestReleaseNotes';
-
-export async function getNewestReleaseNotes(limit = 5) {
-  const resp = await requestWithRetry({ url: NEWEST_PATH, method: 'get', params: { count: limit } });
-  return resp.data || [];
-}
